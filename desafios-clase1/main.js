8d5a5ad15b9322687be2713365dbc56ff881b32f
Juan Cruz Arcucci <juancruzarcucci@hotmail.com>
2021-09-02 19:55:39 -0300

    desafio clase 1

---
+++ desafios-clase1/main.js
@@ -0,0 +1,9 @@
+let num1 = prompt("Primer numero para sumar");
+let num2 = prompt("Segundo numero para sumar");
+
+ num1= parseInt(num1)
+ num2= parseInt(num2)
+
+let resultado = num1+num2;
+
+alert(resultado);
